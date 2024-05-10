b7b76a5e504cd0cd3502c6878189d2f349bec657
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-10 14:59:38 -0700

    Create new collections component

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -9,8 +9,8 @@ export const Popular = () => {
         <h1>POPULAR IN WOMEN</h1>
         <hr />
         <div className="popular-item">
-            {data_product.map((item, i) => {
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            {data_product.map((item, index) => {
+                return <Item key={index} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
             })}
         </div>
     </div>
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Hero } from '../Components/Hero/Hero'
 import { Popular } from '../Components/Popular/Popular'
 import { Offers } from '../Components/Offers/Offers'
+import { NewCollections } from '../Components/NewCollections/NewCollections'
 
 
 export const Shop = () => {
@@ -12,6 +13,7 @@ export const Shop = () => {
         <Hero />
         <Popular />
         <Offers />
+        <NewCollections />
     </div>
   )
 }
