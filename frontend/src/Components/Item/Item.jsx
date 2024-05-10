f52116336d9e334b20ee49d9ae31f7a26626d41c
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-10 14:37:01 -0700

    Completed Popular items component (using the Item component previously created)

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Item.css'
 
 
-
+// used (props) because it's going to be multiple, so we use props.attribute to call the attribute
 export const Item = (props) => {
   return (
     <div className='item'>
@@ -10,10 +10,10 @@ export const Item = (props) => {
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
-                {props.new_price}
+                ${props.new_price}
             </div>
             <div className="item-price-old">
-                {props.old_price}
+                ${props.old_price}
             </div>
         </div>
     </div>
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,12 +2,14 @@
 
 import React from 'react'
 import { Hero } from '../Components/Hero/Hero'
+import { Popular } from '../Components/Popular/Popular'
 
 
 export const Shop = () => {
   return (
     <div>
         <Hero />
+        <Popular />
     </div>
   )
 }
