6fd35b942445cabbb8943e21433f778f59b8af76
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-10 15:27:11 -0700

    Fix runtime error

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Popular.css'
 import data_product from '../Assets/data'
-import Item from '../Item'
+import { Item } from '../Item/Item'
 
 export const Popular = () => {
   return (
