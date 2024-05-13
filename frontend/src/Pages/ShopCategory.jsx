b5e3646bf1a858b9a906dea3d9d0c7715493e617
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-12 22:05:11 -0700

    Create Context element for each category's items (men, women, children)

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,9 +1,36 @@
 // This will be for the separate categories (shop, men, women, kids)
 
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import { Item } from '../Components/Item/Item'
 
-export const ShopCategory = () => {
-  return (
-    <div>ShopCategory</div>
-  )
+export const ShopCategory = (props) => {
+// context is a way to bring in a lot of data from a parent to a child without using too many props
+    const {all_product} = useContext(ShopContext)
+
+    return (
+        <div className='shop-category'>
+            <img className='shopcategory-banner' src={props.banner} alt="" />
+            <div className="shopcategory-indexSort">
+                <p>
+                    <span>Showing 1-12</span> out of 36 products
+                </p>
+                <div className="shopcategory-sort">
+                    Sort by <img src={dropdown_icon} alt="" />
+                </div>
+            </div>
+            <div className="shopcategory-products">
+                {all_product.map((item, index) => {
+                    if (props.cateory === item.category) {
+                        return <Item key={index} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                    }
+                    else {
+                        return null;
+                    }
+                })}
+            </div>
+        </div>
+    )
 }
