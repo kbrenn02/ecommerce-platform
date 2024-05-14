ac08c508ac27b674511aebd85e4bc593bd9f1d92
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-13 22:01:19 -0700

    Clicking a product sends you to the product page

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Item.css'
+import { Link } from 'react-router-dom'
 
 
 // used (props) because it's going to be multiple, so we use props.attribute to call the attribute
 export const Item = (props) => {
   return (
     <div className='item'>
-        <img src={props.image} alt='' />
+        <Link to={`/product/${props.id}`}><img src={props.image} alt='' /></Link>
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
