5f4884d6939d5bbb23317fcce79cc77dfb51dc77
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-14 21:34:17 -0700

    Cart items and total price works

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -5,7 +5,7 @@ import remove_icon from '../Assets/cart_cross_icon.png'
 
 export const CartItems = () => {
 
-    const {all_product, cartItems, removeFromCart} = useContext(ShopContext)
+    const {getTotalCartAmount, all_product, cartItems, removeFromCart} = useContext(ShopContext)
 
     return (
         <div className='cartitems'>
@@ -41,7 +41,7 @@ export const CartItems = () => {
                     <div>
                         <div className="cartitems-total-item">
                             <p>Subtotal:</p>
-                            <p>${0}</p>
+                            <p>${getTotalCartAmount()}</p>
                         </div>
                         <hr />
                         <div className="cartitems-total-item">
@@ -51,7 +51,7 @@ export const CartItems = () => {
                         <hr />
                         <div className="cartitems-total-item">
                             <h3>Total</h3>
-                            <h3>${0}</h3>
+                            <h3>${getTotalCartAmount()}</h3>
                         </div>
                     </div>
                     <button>PROCEED TO CHECKOUT</button>
