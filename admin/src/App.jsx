152724b5b321d5adebfbe424494a870927d108b7
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-19 18:30:25 -0700

    Add components and remove errors

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Navbar from './Components/NavBar/Navbar.jsx'
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
 
 
 const App = () => {
   return (
     <div>
         <Navbar />
+        <Admin />
     </div>
   )
 }
