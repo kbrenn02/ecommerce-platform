b98ef0e9a55ddf3ea107c74061ccabae18414176
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-19 21:28:31 -0700

    Making frontend login/signup page reactive to whether a user is trying to do one action or the other

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
-  return (
-    <div className='loginsignup'>
-        <div className="loginsignup-container">
-            <h1>Sign Up</h1>
-            <div className="loginsignup-fields">
-                <input type="text" placeholder='Your Name' />
-                <input type="email" placeholder='Email Address' />
-                <input type="password" placeholder='Password'/>
-            </div>
-            <button>Continue</button>
-            <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
-            <div className="loginsignup-agree">
-                <input type="checkbox" name='' id='' />
-                <p>By continuing, I agree to the terms of use & privacy policy.</p>
+
+    const [state, setState] = useState("Login");
+    
+
+    const login = async () => {
+        console.log("Login function executed")
+    }
+
+    const signup = async () => {
+        console.log("signup function executed")
+    }
+
+
+    return (
+        <div className='loginsignup'>
+            <div className="loginsignup-container">
+                <h1>{state}</h1>
+                <div className="loginsignup-fields">
+                    {state === 'Sign Up' ? <input type="text" placeholder='Your Name' /> : <></>}
+                    <input type="email" placeholder='Email Address' />
+                    <input type="password" placeholder='Password'/>
+                </div>
+                <button onClick={ () => 
+                    {state === 'Login' ? 
+                        login() : 
+                        signup()}}>Continue</button>
+                {state === 'Sign Up' ? 
+                    <p className="loginsignup-login">Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p> :
+                    <p className="loginsignup-login">Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>
+                }
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name='' id='' />
+                    <p>By continuing, I agree to the terms of use & privacy policy.</p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
